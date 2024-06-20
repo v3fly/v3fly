d5700ff02a34a852587c0cecaac9f746d1a3ed4c
v3fly <liao532945wang248@163.com>
2024-06-21 02:09:05 +0800

    Insertion d'une navbar de menu pour le profil collaborateur #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,33 @@ import {StatutConnecteService} from './auth/statut-connecte.service';
 import {AuthInterceptorService} from './auth/auth-interceptor.service';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
+import { ReservationsComponent } from './Menu/reservations/reservations.component';
+import { AnnoncesComponent } from './Menu/annonces/annonces.component';
+import { StatistiquesComponent } from './Menu/statistiques/statistiques.component';
 
+export const ROUTES: Routes = [
+  { path: 'collaborateur/reservations', component: ReservationsComponent},
+  { path: 'collaborateur/annonces', component: AnnoncesComponent},
+  { path: 'collaborateur/statistiques', component: StatistiquesComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    TechComponent
+    TechComponent,
+    MenuComponent,
+    ReservationsComponent,
+    AnnoncesComponent,
+    StatistiquesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
