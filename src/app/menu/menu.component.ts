14fa6baebcca19b2ca96044ee9fa118f8251f338
v3fly <liao532945wang248@163.com>
2025-01-18 20:29:46 +0800

    Design menu #3

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Collegue } from '../auth/auth.domains';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -9,15 +10,21 @@ import { AuthService } from '../auth/auth.service';
 })
 export class MenuComponent implements OnInit {
 
+  collegueCo: Collegue
 
   constructor(private activatedRoute: ActivatedRoute, private authSrv: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.recupCollegueCo();
   }
 
   seDeconnecter() {
     this.authSrv.seDeconnecter().subscribe();
     this.router.navigateByUrl("/connexion");
+  }
+
+  recupCollegueCo() {
+    return this.authSrv.collegueConnecteObs.subscribe(col => this.collegueCo = col)
   }
 
 }
