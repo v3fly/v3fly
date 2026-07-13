d4801d5ec2f8d5006e408ca5f99e49df7920d028
v3fly <liao532945wang248@163.com>
2026-07-14 04:03:48 +0800

    Rubrique covoit repliable (dépliée par défaut) + bouton retour à la liste actif #5

--- src/app/menu/reservations-collab/creer/creer.component.ts
+++ src/app/menu/reservations-collab/creer/creer.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreerComponent implements OnInit {
 
+  covoitDeroule = true;
+  societeDeroule = false;
+  chauffeurDeroule = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  deroulerCovoit(){
+    this.chauffeurDeroule = false;
+    this.societeDeroule = false;
+    this.covoitDeroule = true;
+  }
+
+  deroulerChauffeur(){
+    this.chauffeurDeroule = true;
+    this.societeDeroule = false;
+    this.covoitDeroule = false;
+  }
+
+  deroulerSociete(){
+    this.chauffeurDeroule = false;
+    this.societeDeroule = true;
+    this.covoitDeroule = false;
+  }
 }
